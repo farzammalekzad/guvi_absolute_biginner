61e2470e43cb15a0f623351a91acea67ef9fea8c
Farzam Malekzad <mohammad.malekzad@yahoo.com>
2020-04-04 04:54:01 +0430

    STDIN

---
+++ Absolute Biginner/for STDIN/STDIN.js
@@ -0,0 +1,19 @@
+/*
+const readline = require("readline");
+
+const inp = readline.createInterface({
+    input: process.stdin
+});
+let lines = [];
+inp.on("line", (data) => {
+
+    lines.push(data);
+});
+inp.on('close', () => {
+
+
+       codes writes HERE
+
+    console.log(lines[0]);
+});
+*/
